e8f47d957918ca33c353bbac4825b61fa2a08c94
Marcin Lech <arcinech@gmail.com>
2022-07-11 14:25:50 +0200

    Add: Routes for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Homepage from "./components/pages/Homepage/Homepage";
+import Container from 'react-bootstrap/Container';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Header from './components/views/Header/Header';
+import Home from "./components/pages/Home/Home";
+import Projects from './components/pages/Projects/Projects';
+import NotFound from './components/pages/NotFound/NotFound';
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects/> } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
+
+//styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/custom.scss';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
