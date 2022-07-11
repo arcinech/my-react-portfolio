b6958c1a900f99b236c9a9742c9e0a37b6004fb8
Marcin Lech <arcinech@gmail.com>
2022-07-11 11:30:50 +0200

    Add: homepage structure

---
+++ src/components/pages/Homepage/Homepage.jsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+
+  return (
+    <>
+      <h3>My Portfolio</h3>
+    </>
+  )
+};
+
+export default Homepage;
---
+++ src/components/pages/Homepage/Homepage.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import Homepage from './Homepage';
+
+test('render react Homepage', () => {
+  render(<Homepage />);
+});
