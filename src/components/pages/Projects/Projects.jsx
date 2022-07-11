9ff288e92080678cd61dd7a4a74c77c0b7ab2f91
Marcin Lech <arcinech@gmail.com>
2022-07-11 14:35:11 +0200

    Add: Projects component

---
+++ src/components/pages/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import ProjectList from "../../features/ProjectsList/ProjectsList";
+
+const Projects = () => {
+  return (
+    <section className='container'>
+      <h1>My Projects</h1>
+      <ProjectList />
+    </section>
+  );
+};
+
+export default Projects;
---
+++ src/components/pages/Projects/Projects.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import Projects from './Projects';
+
+test('render react Projects', () => {
+  render(<Projects />);
+});
