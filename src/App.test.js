03a3499a0f473f25013934bfed55b3c8ecc3a104
Marcin Lech <arcinech@gmail.com>
2022-07-11 14:34:33 +0200

    Fix: app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 
 test('render react App', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 });
