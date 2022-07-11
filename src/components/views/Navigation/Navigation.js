0c94d66689266fbe9de3ed58c8881142c6347e6d
Marcin Lech <arcinech@gmail.com>
2022-07-11 14:35:27 +0200

    Add: ProjectsList test

--- src/components/views/Navigation/Navigation.js
+++ src/components/views/Navigation/Navigation.js
@@ -12,7 +12,7 @@ const Navigation = () => {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link as={Link} to='/'>Home</Nav.Link>
-          <Nav.Link as={Link} to='/projects'>My projects</Nav.Link>
+          <Nav.Link as={Link} to='w/projects'>My projects</Nav.Link>
         </Nav>
       </Navbar.Collapse>
       </Container>
