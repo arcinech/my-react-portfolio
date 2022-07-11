f5b03c17dbd5b0076a9afec0bdf50e21c907ac8a
Marcin Lech <arcinech@gmail.com>
2022-07-11 14:26:35 +0200

    Add: Work on ProjectCard

--- src/components/common/ProjectCard/ProjectCard.jsx
+++ src/components/common/ProjectCard/ProjectCard.jsx
@@ -1,7 +1,18 @@
-const ProjectCard = props => {
+import Button from 'react-bootstrap/Button'
+import styles from './ProjectCard.module.scss';
+
+const ProjectCard = ({id, name, github, demo}) => {
   return (
-    <>
-    </>
+    <div key={id} className={styles.root}>
+      <div>{name}</div>
+      <div className={styles.info}>
+        {/* <img src='#' alt={name}/> */}
+        <div className={styles.buttons}>
+          <Button variant="danger" href="demo" target="_blank">DEMO</Button>
+          <Button variant="info" href="github" target="_blank">GITHUB</Button>
+        </div>
+      </div>
+    </div>
   )
 };
 
