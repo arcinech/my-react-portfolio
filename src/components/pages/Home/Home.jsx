0870a22c0ebc5ca4144378e54449c33b4041a8fc
Marcin Lech <arcinech@gmail.com>
2022-07-19 15:44:09 +0200

    Change home behavior

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import About from '../../features/About/About';
+import TechIcons from '../../common/TechIcons/TechIcons';
+import {faHtml5, faCss3, faJsSquare, faReact, faNodeJs} from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
+
+  const icons = [{icon: faHtml5, name: 'HTML5'}, {icon: faCss3, name: 'CSS3'}, {icon: faJsSquare, name: 'JavaScript'}, {icon: faReact, name: 'React'}, {icon: faNodeJs, name: 'NodeJS'}];
 
   return (
     <section>
       <h3>My Portfolio</h3>
       <About />
+      <TechIcons icons={icons}/>
     </section>
   )
 };
